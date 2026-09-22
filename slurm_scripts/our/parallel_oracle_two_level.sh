9e3efd068b06a39002ff0d886bf139e9552e6ae9
Andrey andronenko <avandronenko@avito.com>
2026-09-22 22:05:56 +0300

    config

---
+++ slurm_scripts/our/parallel_oracle_two_level.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -euo pipefail
+
+name_part="oracle_two_level"
+
+/s/ls4/users/dartl0l/goarl/rl_a3c_slurm/slurm_scripts/our/parallel_oracle_runner.sh "$@" \
+  "${name_part}" \
+  /s/ls4/users/dartl0l/goarl/rl_a3c_slurm/configs/run_config_oracle_two_level.ini
